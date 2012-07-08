f6138b95d997bab7f775b3a54863d1a5a1a58419
hbristow <hilton.bristow@gmail.com>
2012-07-07 17:24:36 -0700

    Updated Parts.hpp interface

--- include/Parts.hpp
+++ include/Parts.hpp
@@ -40,80 +40,98 @@
 #define PARTS_HPP_
 #include <assert.h>
 
+typedef std::vector<int>                   vectori;
+typedef std::vector<float>				   vectorf;
+typedef std::vector<cv::Mat>               vectorMat;
 typedef std::vector<std::vector<cv::Mat> > vector2DMat;
 typedef std::vector<std::vector<float> >   vector2Df;
 typedef std::vector<std::vector<int> >     vector2Di;
+typedef std::vector<vector2Di>             vector3Di;
 
 
-class ComponentParts {
+class ComponentPart {
 private:
-	std::vector<cv::Mat>& filtersw_;
-	std::vector<int>&     filtersi_;
-	vector2Df&            defw_;
-	std::vector<int>&     defi_;
-	vector2Di&            biasid_;
-	vector2Di&            filterid_;
-	std::vector<int>&     parentid_;
-	int                   self_;
+	vectorMat const * filtersw_;
+	vectori   const * filtersi_;
+	vectorf   const * biasw_;
+	vectori   const * biasi_;
+	vector2Df const * defw_;
+	vectori   const * defi_;
+	vector2Df const * anchors_;
+	vector2Di const * biasid_;
+	vector2Di const * filterid_;
+	vectori   const * parentid_;
+	int               self_;
 public:
-	ComponentParts(std::vector<cv::Mat>& filtersw, std::vector<int>& filtersi, vector2Df& defw, std::vector<int>& defi,
-				   vector2Di& biasid, vector2Di& filterid, std::vector<int>& parentid, int self) :
-	   filtersw_(filtersw), filtersi_(filtersi), filterid_(filterid_),
-		defw_(defw), defi_(defi), biasid_(biasid),
-		parentid_(parentid), self_(self) {}
-	ComponentParts(const ComponentParts& other, int self) :
+	ComponentPart() {}
+	ComponentPart(const vectorMat& filtersw, const vectori& filtersi,
+			       const vectorf& biasw, const vectori& biasi, const vector2Df& anchors,
+			       const vector2Df& defw, const vectori& defi,
+				   const vector2Di& biasid, const vector2Di& filterid, const vectori& parentid, int self) :
+	   filtersw_(&filtersw), filtersi_(&filtersi), filterid_(&filterid),
+	    biasw_(&biasw), biasi_(&biasi), anchors_(&anchors),
+		defw_(&defw), defi_(&defi), biasid_(&biasid),
+		parentid_(&parentid), self_(self) {}
+	ComponentPart(const ComponentPart& other, int self) :
 		filtersw_(other.filtersw_), filtersi_(other.filtersi_), filterid_(other.filterid_),
+		biasw_(other.biasw_), biasi_(other.biasi_), anchors_(other.anchors_),
 		defw_(other.defw_), defi_(other.defi_), biasid_(other.biasid_),
 		parentid_(other.parentid_), self_(self) {}
-	virtual ~ComponentParts() {}
-	// assignment operator
-	ComponentParts& operator=(const ComponentParts& rhs) {
-		if (&rhs == this) return *this;
-		filtersw_ = rhs.filtersw_;
-		filtersi_ = rhs.filtersi_;
-		defw_     = rhs.defw_;
-		defi_     = rhs.defi_;
-		biasid_   = rhs.biasid_;
-		filterid_ = rhs.filterid_;
-		parentid_ = rhs.parentid_;
-		self_     = rhs.self_;
-		return *this;
-	}
+	virtual ~ComponentPart() {}
 	// get methods
-	int nparts(void) const { return filterid_.size(); }
+	int nparts(void) const { return filterid_->size(); }
 	int nmixtures(void) const { return filterid_[self_].size(); }
 	int self(void) const { return self_; }
 	// perform translation of indices internally
-	const cv::Mat& filter(int mixture) const { assert(filterid_[self_].size() > mixture); return filtersw_[filterid_[self_][mixture]]; }
-	const std::vector<cv::Mat> filters(void) const {
-		std::vector<cv::Mat> out;
-		for (int m = 0; m < filterid_[self_].size(); ++m) out.push_back(filtersw_[filterid_[self_][m]]);
+	const cv::Mat& filter(int mixture = 0) const {
+		assert((*filterid_)[self_].size() > mixture);
+		return (*filtersw_)[(*filterid_)[self_][mixture]]; }
+	const vectorMat filters(void) const {
+		vectorMat out;
+		for (int m = 0; m < filterid_[self_].size(); ++m) {
+			out.push_back((*filtersw_)[(*filterid_)[self_][m]]);
+		}
 		return out;
 	}
-	ComponentParts parent(void) const { return ComponentParts(*this, parentid_[self_]); }
-	std::vector<ComponentParts> children(void) const {
-		std::vector<ComponentParts> c;
-		for (int n = self_; n < parentid_.size(); ++n) {
-			ComponentParts cp(*this, n);
-			if (parentid_[n] == self_) c.push_back(cp);
+	ComponentPart parent(void) const { return ComponentPart(*this, (*parentid_)[self_]); }
+	std::vector<ComponentPart> children(void) const {
+		std::vector<ComponentPart> c;
+		for (int n = self_; n < parentid_->size(); ++n) {
+			ComponentPart cp(*this, n);
+			if ((*parentid_)[n] == self_) c.push_back(cp);
 		}
 		return c;
 	}
+	cv::Mat& score(vectorMat& scores, int mixture = 0) const {
+		assert((*filterid_)[self_].size() > mixture);
+		return scores[(*filterid_)[self_][mixture]];
+	}
+	int filteri(int mixture = 0) const { return (*filtersi_)[(*filterid_)[self_][mixture]]; }
+	float bias(int mixture = 0) const { return (*biasw_)[(*biasid_)[self_][mixture]]; }
+	int biasi(int mixture = 0) const { return (*biasi_)[(*biasid_)[self_][mixture]]; }
 };
 
 class Parts {
 private:
-	std::vector<cv::Mat> filtersw_;
-	std::vector<int>     filtersi_;
-	vector2Df            defw_;
-	std::vector<int>     defi_;
-	std::vector<float>   biasw_;
-	std::vector<int>     biasi_;
+	// common monolithic part components
+	vectorMat filtersw_;
+	vectori   filtersi_;
+	vector2Df defw_;
+	vectori   defi_;
+	vectorf   biasw_;
+	vectori   biasi_;
 	vector2Df anchors_;
-	std::vector<ComponentParts> components_;
+	// component accessors
+	vector3Di biasid_;
+	vector3Di filterid_;
+	vector2Di parentid_;
 public:
 	Parts();
 	virtual ~Parts();
+	ComponentPart component(int n) const {
+		assert(n < biasid_.size() && n < filterid_.size() && n < parentid_.size());
+		return ComponentPart(filtersw_, filtersi_, biasw_, biasi_, anchors_, defw_, defi_, biasid_[n], filterid_[n], parentid_[n], 0);
+	}
 };
 
 #endif /* PARTS_HPP_ */
